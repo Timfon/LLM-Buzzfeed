3ea328ee42a2349ea0c5c1bd731c410effa70dd8
MG2003 <max.guo2014@gmail.com>
2023-10-21 12:58:25 -0400

    routing

--- frontend/src/layout/RootLayout.tsx
+++ frontend/src/layout/RootLayout.tsx
@@ -2,9 +2,19 @@ import { Outlet } from "react-router-dom";
 
 export default function Root() {
     return (
-      <div>
+      <div className = "p-2">
+        <Head/>
         <p>Home</p>
         <Outlet/>
       </div>
     )
   }
+
+
+function Head() {
+  return (
+    <div>
+        LLM BUZZFEED!
+    </div>
+  )
+}
